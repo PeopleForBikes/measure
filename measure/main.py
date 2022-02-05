fdcdc93f93d00d1d40cccef1de935d1d34dd3d99
Rémy Greinhofer <remy.greinhofer@gmail.com>
2022-02-05 17:10:21 -0600

    Add CSV output
    Export the results to a CSV file.

--- measure/main.py
+++ measure/main.py
@@ -1,5 +1,5 @@
 """Define the main module."""
-import json
+import csv
 from pathlib import Path
 
 import geopandas
@@ -48,7 +48,7 @@ def main():
     shapefiles = list(p.glob("data/*neighborhood_ways.zip"))
 
     # Process them.
-    results = {}
+    results = []
     for shapefile in shapefiles:
         # Load the shapefile.
         raw_gdf = geopandas.read_file(shapefile)
@@ -62,10 +62,23 @@ def main():
         # Get the distances in km.
         grouped["distance"] = grouped["distance"].astype(int) / 1000
 
-        # Display the results.
-        results[shapefile.stem] = grouped.to_dict("index")
+        # Save the results.
+        grouped_results = grouped.to_dict()["distance"]
+        country, state, city, _ = shapefile.stem.split("-")
+        grouped_results["country"] = country
+        grouped_results["state"] = state
+        grouped_results["city"] = city
+        results.append(grouped_results)
 
-    print(json.dumps(results, indent=2))
+    # Display the results as JSON.
+    # print(json.dumps(results, indent=2))
+
+    # Display the results as CSV.
+    print(results)
+    with open("results/measure.csv", "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=results[0].keys())
+        writer.writeheader()
+        writer.writerows(results)
 
 
 if __name__ == "__main__":
